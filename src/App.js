63e2696d7a18ca494a2736efcddd6b98039a7cf7
Harvey Granfil <jhgranfil@gmail.com>
2021-06-05 02:39:40 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import GlobalStyle from "./components/styled-components/globalStyles";
+import particles from "./particles";
+
+import {
+  Particles,
+  Wrapper,
+} from "./components/styled-components/styledComponents";
 
 function App() {
+  useEffect(() => {
+    window.particlesJS("particles-js", particles);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <Particles id="particles-js" />
+    </Wrapper>
   );
 }
 
---
+++ src/project-data.js
@@ -0,0 +1,73 @@
+import battleshipImage1 from "./assests/battleship/battleship-1.png";
+import battleshipImage2 from "./assests/battleship/battleship-2.png";
+import battleshipImage3 from "./assests/battleship/battleship-3.png";
+
+import shoppingCartImage1 from "./assests/shopping-cart/shopping-cart-1.png";
+import shoppingCartImage2 from "./assests/shopping-cart/shopping-cart-2.png";
+import shoppingCartImage3 from "./assests/shopping-cart/shopping-cart-3.png";
+import shoppingCartImage4 from "./assests/shopping-cart/shopping-cart-4.png";
+
+import memoryGameImage1 from "./assests/memory-game/memory-game-1.png";
+import memoryGameImage2 from "./assests/memory-game/memory-game-2.png";
+
+import cvMakerImage1 from "./assests/cv-app/cv-app-1.png";
+import cvMakerImage2 from "./assests/cv-app/cv-app-2.png";
+import cvMakerImage3 from "./assests/cv-app/cv-app-3.png";
+
+import weatherImage1 from "./assests/weather-app/weather-1.png";
+import weatherImage2 from "./assests/weather-app/weather-2.png";
+
+import restaurantImage1 from "./assests/restaurant-page/restaurant-1.png";
+import restaurantImage2 from "./assests/restaurant-page/restaurant-2.png";
+import restaurantImage3 from "./assests/restaurant-page/restaurant-3.png";
+
+const projectData = [
+  {
+    name: "Battleship",
+    live: "https://harvok17.github.io/battleship/",
+    repo: " https://github.com/Harvok17/battleship",
+    images: [battleshipImage1, battleshipImage2, battleshipImage3],
+  },
+
+  {
+    name: "Shopping Cart",
+    live: "https://harvok17.github.io/shopping-cart/#/",
+    repo: "https://github.com/Harvok17/shopping-cart",
+    images: [
+      shoppingCartImage1,
+      shoppingCartImage2,
+      shoppingCartImage3,
+      shoppingCartImage4,
+    ],
+  },
+
+  {
+    name: "Memory Game",
+    live: "https://harvok17.github.io/memory-game/",
+    repo: "https://github.com/Harvok17/memory-game",
+    images: [memoryGameImage1, memoryGameImage2],
+  },
+
+  {
+    name: "CV Maker",
+    live: "https://harvok17.github.io/cv-app/",
+    repo: "https://github.com/Harvok17/cv-app",
+    images: [cvMakerImage1, cvMakerImage2, cvMakerImage3],
+  },
+
+  {
+    name: "Weather app",
+    live: "https://harvok17.github.io/weather-app/",
+    repo: "https://github.com/Harvok17/weather-app",
+    images: [weatherImage1, weatherImage2],
+  },
+
+  {
+    name: "Restaurant page",
+    live: "https://harvok17.github.io/restaurant-page/",
+    repo: "https://github.com/Harvok17/restaurant-page",
+    images: [restaurantImage1, restaurantImage2, restaurantImage3],
+  },
+];
+
+export default projectData;
